c469c6a72edb95686ad03fe123de91d318250370
Amy Greene <amygreene@gmail.com>
2013-10-09 11:50:31 -0700

    initial commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+//replace url in wp_register() to point to network_home register page
+add_action( 'register' , 'register_replacement' );
+function register_replacement( $link ){
+	if ( ! is_user_logged_in() ) {
+		if ( get_option('users_can_register') )
+			$link = $before . '<a href="' . network_home_url('register', 'login') . '">' . __('Register') . '</a>' . $after;
+		else
+			$link = '';
+	} else {
+		$link = $before . '<a href="' . admin_url() . '">' . __('Site Admin') . '</a>' . $after;
+	}
+	return $link;
+}
+?>
